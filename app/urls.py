52632f22902c7c87be9ce4457ad8810f8e91f992
Flavio Macedo <fhenrique609@gmail.com>
2023-12-13 22:08:34 -0300

    Exportar CSV

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('gerar_relatorio_pdf/', views.gerar_relatorio_pdf, name='gerar_relatorio_pdf'),
+    path('exportar_csv/', views.exportar_csv, name='exportar_csv'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from weasyprint import HTML
+
 import os
+import csv
 
 
 def home(request):
@@ -24,6 +26,36 @@ def gerar_relatorio_pdf(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'filename="relatorio.pdf"'
     response.write(pdf_files)
+
+    return response
+
+
+def exportar_csv(request):
+    # Dados da tabela
+    dados = [
+        {'NOME': 'Alex Pereira', 'SETOR': 'FNDE', 'MUNICÍPIO': 'Pedro Avelino', 'ATIVIDADE': 'Prestação de Contas', 'DESCRIÇÃO DA ATIVIDADE': 'PPDE 2022 das escolas do município', 'DATA DE RECEPÇÃO': '16/03/2023', 'DATA DE INICIO': '06/07/2023', 'DATA DO FIM': '28/11/2023', 'DURAÇÃO DE DIAS': 104, 'OBSERVAÇÃO': '', 'STATUS': 'Concluído'},
+        {'NOME': 'Anaise', 'SETOR': 'FNDE', 'MUNICÍPIO': 'Touros', 'ATIVIDADE': 'Prestação de Contas', 'DESCRIÇÃO DA ATIVIDADE': 'PNAE 2022', 'DATA DE RECEPÇÃO': '28/04/2023', 'DATA DE INICIO': '28/04/2023', 'DATA DO FIM': '', 'DURAÇÃO DE DIAS': '', 'OBSERVAÇÃO': 'Está sendo lançado o mês de dezembro de 2022 no sistema', 'STATUS': 'Pendente'},
+        {'NOME': 'Maryllia', 'SETOR': 'Receita Federal', 'MUNICÍPIO': 'Rio do Fogo', 'ATIVIDADE': 'RFB - Parcelamento', 'DESCRIÇÃO DA ATIVIDADE': 'Formalização do parcelamento', 'DATA DE RECEPÇÃO': '', 'DATA DE INICIO': '', 'DATA DO FIM': '', 'DURAÇÃO DE DIAS': '', 'OBSERVAÇÃO': 'Ordem Cronologica', 'STATUS': 'Não iniciado'},
+        {'NOME': 'Marta', 'SETOR': 'FTGS', 'MUNICÍPIO': 'Tibau do Sul', 'ATIVIDADE': 'Individualizar', 'DESCRIÇÃO DA ATIVIDADE': '', 'DATA DE RECEPÇÃO': '01/06/2023', 'DATA DE INICIO': '01/06/2023', 'DATA DO FIM': '', 'DURAÇÃO DE DIAS': '', 'OBSERVAÇÃO': 'Aguardando documentos. Só veio extrato', 'STATUS': 'Pendente'},
+        {'NOME': 'Cinthia', 'SETOR': 'CAIXA', 'MUNICÍPIO': 'São Miguel do Gostoso', 'ATIVIDADE': 'Prestação de Contas', 'DESCRIÇÃO DA ATIVIDADE': 'SICONV', 'DATA DE RECEPÇÃO': '27/10/2023', 'DATA DE INICIO': '30/10/2023', 'DATA DO FIM': '05/11/2023', 'DURAÇÃO DE DIAS': '5', 'OBSERVAÇÃO': 'Enviar ao BJPREV', 'STATUS': 'Concluído'},
+        {'NOME': 'Daniel Molick', 'SETOR': 'CAIXA', 'MUNICÍPIO': 'Extremoz', 'ATIVIDADE': 'Prestação de Contas', 'DESCRIÇÃO DA ATIVIDADE': 'SICONV', 'DATA DE RECEPÇÃO': '27/10/2023', 'DATA DE INICIO': '05/11/2023', 'DATA DO FIM': '', 'DURAÇÃO DE DIAS': '', 'OBSERVAÇÃO': '', 'STATUS': 'Em Analise'},
+        {'NOME': 'Fátima', 'SETOR': 'Contabilidade', 'MUNICÍPIO': 'Goianinha', 'ATIVIDADE': 'Auditoria', 'DESCRIÇÃO DA ATIVIDADE': 'Contabilidade dos centro de custos', 'DATA DE RECEPÇÃO': '28/10/2023', 'DATA DE INICIO': '10/11/2023', 'DATA DO FIM': '', 'DURAÇÃO DE DIAS': '', 'OBSERVAÇÃO': '', 'STATUS': 'Em Analise'},
+        {'NOME': 'Ingrid', 'SETOR': 'SIMEC', 'MUNICÍPIO': 'Lagoa Salgada', 'ATIVIDADE': 'Envio de documento', 'DESCRIÇÃO DA ATIVIDADE': 'Documento para celebração', 'DATA DE RECEPÇÃO': '30/10/2023', 'DATA DE INICIO': '15/12/2023', 'DATA DO FIM': '15/12/2023', 'DURAÇÃO DE DIAS': '1', 'OBSERVAÇÃO': '', 'STATUS': 'Concluído'},
+    ]
+
+    # Configuração do response para um arquivo CSV
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="export.csv"'
+
+    # Cria um objeto CSVWriter
+    writer = csv.writer(response)
+
+    # Escreve os cabeçalhos
+    writer.writerow(['NOME', 'SETOR', 'MUNICÍPIO', 'ATIVIDADE', 'DESCRIÇÃO DA ATIVIDADE', 'DATA DE RECEPÇÃO', 'DATA DE INICIO', 'DATA DO FIM', 'DURAÇÃO DE DIAS', 'OBSERVAÇÃO', 'STATUS'])
+
+    # Escreve os dados
+    for dado in dados:
+        writer.writerow([dado['NOME'], dado['SETOR'], dado['MUNICÍPIO'], dado['ATIVIDADE'], dado['DESCRIÇÃO DA ATIVIDADE'], dado['DATA DE RECEPÇÃO'], dado['DATA DE INICIO'], dado['DATA DO FIM'], dado['DURAÇÃO DE DIAS'], dado['OBSERVAÇÃO'], dado['STATUS']])
 
     return response
     
